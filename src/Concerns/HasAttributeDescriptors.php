c83934d32081165e2c0e57f5baf5bf3c7b1fe287
Dan Matthews <dan@danmatthews.me>
2024-08-28 21:51:38 +0100

    Remove dependency on collect() function

--- src/Concerns/HasAttributeDescriptors.php
+++ src/Concerns/HasAttributeDescriptors.php
@@ -3,6 +3,7 @@
 namespace Intrfce\EnumAttributeDescriptors\Concerns;
 
 use Intrfce\EnumAttributeDescriptors\Attributes\Description;
+use Intrfce\EnumAttributeDescriptors\Attributes\MetaData;
 use Intrfce\EnumAttributeDescriptors\Attributes\Title;
 use ReflectionClassConstant;
 
@@ -11,16 +12,46 @@ trait HasAttributeDescriptors
     public function getTitle(): ?string
     {
         $reflectionClass = new ReflectionClassConstant($this, $this->name);
-        $attribute = collect($reflectionClass->getAttributes(Title::class));
-
-        return $attribute->first()?->newInstance()?->title;
+        $attributes = $reflectionClass->getAttributes(Title::class);
+        return isset($attributes[0]) ? $attributes[0]?->newInstance()?->title : $this->titleFallback();
     }
 
     public function getDescription(): ?string
     {
         $reflectionClass = new ReflectionClassConstant($this, $this->name);
-        $attribute = collect($reflectionClass->getAttributes(Description::class));
+        $attributes = $reflectionClass->getAttributes(Description::class);
+        return isset($attributes[0]) ? $attributes[0]?->newInstance()?->description : $this->descriptionFallback();
+    }
 
-        return $attribute->first()?->newInstance()?->description;
+
+    /**
+     * Returns and MetaData you have set against the enum case.
+     * The MetaData class accepts an array of data.
+     *
+     * @return mixed
+     */
+    public function getMetaData(): mixed
+    {
+        $reflectionClass = new ReflectionClassConstant($this, $this->name);
+        $attributes = $reflectionClass->getAttributes(MetaData::class);
+        return isset($attributes[0]) ? $attributes[0]->newInstance()?->description : null;
+    }
+
+    /**
+     * Override this in your class to edit the fallback.
+     * @return string|null
+     */
+    public function titleFallback(): string|null
+    {
+        return null;
+    }
+
+    /**
+     * Override this in your class to edit the fallback.
+     * @return string|null
+     */
+    public function descriptionFallback(): string|null
+    {
+        return null;
     }
 }
